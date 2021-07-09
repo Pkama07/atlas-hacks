6ad51d88d4e84dd27b59e7396f92fe1b4768717c
Pradyun Kamaraju <49815607+Pkama07@users.noreply.github.com>
2021-07-08 20:22:34 -0400

    Update popup.js

--- extension/scripts/popup.js
+++ extension/scripts/popup.js
@@ -1,37 +1,3 @@
-/*
-chrome.storage.local.get("blockedWebsites", ({ blockedWebsites }) => {
-    for(website of blockedWebsites) {
-        var listNode = document.getElementById("blocked");
-        var liNode = document.createElement("LI");
-        var textNode = document.createTextNode(website);
-        liNode.append(textNode);
-        listNode.append(liNode);
-    }
-});
-
-const addWebsite = () => {
-
-    var website = document.getElementById("website_input").value;
-
-    chrome.storage.local.get("blockedWebsites", ({ blockedWebsites }) => {
-        blockedWebsites.push(website);
-        chrome.storage.local.set({"blockedWebsites": blockedWebsites });
-    });
-
-
-
-    var listNode = document.getElementById("blocked");
-    var liNode = document.createElement("LI");
-    var textNode = document.createTextNode(website);
-    
-    liNode.append(textNode);
-    listNode.append(liNode);
-    document.getElementById("website_input").value = "";
-
-};
-document.getElementById("addBtn").addEventListener("click", addWebsite);
-*/
-
 function generateList() {
     var myList = document.getElementById('myList');
     myList.innerHTML = '';
